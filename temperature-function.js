2594a14c17a730d6cefbc2f0ac118bf0bf011824
Marcell Hayes <marcell.made@gmail.com>
2017-04-29 23:15:34 -0400

    js changed. unfin. css gridd added. practicing animations unfin.

--- temperature-function.js
+++ temperature-function.js
@@ -1,11 +1,35 @@
-function convertTemp(temp,unit) {
-  if ( unit === "C") {
-  console.log((temp * 9/5) + 32);
+$('#convert').on('click', function() {
+
+//var button = document.body.getElementsByTagName('button');
+//button.onclick = function(){
+//    alert("clicked");
+//}
+
+function convertTemp() {
+    
+    var temperature = $('#temperature').val;
+    var tempUnit = $('#temp-unit').val;
+    
+  if ( tempUnit === "C") {
+      console.log("done1!");
+  (temperature * 9/5) + 32;
+      document.body.innerHTML('<div><p>Your temperature converts to ' + temperature + ' degrees ' + tempUnit + ' .');
 }else{
-  console.log(temp-32) * 5/9;
+    console.log("done2!");
+  (temperature-32) * 5/9;
+    document.body.innerHTML('<div><p>Your temperature converts to ' + temperature + ' degrees ' + tempUnit + ' .');
   }
-}
-convertTemp(50,"C");
+};
+    
+convertTemp();    
+            
+});
+
+//convertTemp(50,"C");
+
+
+
 
 // (C × 9/5) + 32 = F;
 // (F − 32) x 5/9 = C;
+
