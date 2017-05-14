fb7414c29ae45be95a47f9acd47eb12baed36bbe
Marcell Hayes <marcell.made@gmail.com>
2017-05-14 14:10:33 -0400

    hindsight changes to js and css

--- temperature-function.js
+++ temperature-function.js
@@ -4,20 +4,29 @@ $('#convert').on('click', function() {
 //button.onclick = function(){
 //    alert("clicked");
 //}
-
-function convertTemp() {
     
-    var temperature = $('#temperature').val;
-    var tempUnit = $('#temp-unit').val;
+  
+
+    
+function convertTemp(temp, tempUnit) {
+    
+    var temp = $('#temp').val();
+    var tempUnit = $('#temp-unit').val();  
     
   if ( tempUnit === "C") {
       console.log("done1!");
-  (temperature * 9/5) + 32;
-      document.body.innerHTML('<div><p>Your temperature converts to ' + temperature + ' degrees ' + tempUnit + ' .');
+    newTemp = Math.floor((temp * 9/5) + 32);
+      document.getElementById('message').innerHTML = '<div><p>Your temperature converts to ' + newTemp + '&deg;' + 'F </p></div>';
+      
+      bgSwap(newTemp);
+      
 }else{
     console.log("done2!");
-  (temperature-32) * 5/9;
-    document.body.innerHTML('<div><p>Your temperature converts to ' + temperature + ' degrees ' + tempUnit + ' .');
+  newTemp = Math.floor((temp-32) * 5/9);
+    document.getElementById('message').innerHTML = '<div><p>Your temperature converts to ' + newTemp + '&deg;' + 'C </p></div>';
+    
+    bgSwap(temp);
+    
   }
 };
     
@@ -25,9 +34,27 @@ convertTemp();
             
 });
 
+function bgSwap(num) {
+    if ( num < 40 ) {
+        document.getElementById("right-side").style.backgroundImage = "url(https://s-media-cache-ak0.pinimg.com/564x/59/85/03/598503b3b961afbbe2fbd236f4d36dd6.jpg)";
+        
+        document.getElementById("message").style.textShadow = "5pt 3pt 5pt #000aff";
+        
+        document.getElementById("convert").style.animationName = "color-change-cool";
+        
+        
+    } else if ( num > 75 ) {
+        document.getElementById("right-side").style.backgroundImage = "url(https://s-media-cache-ak0.pinimg.com/564x/cd/44/f4/cd44f4373fbbf40adabe9e01ac68f5c2.jpg)";
+        
+        document.getElementById("message").style.textShadow = "5pt 3pt 5pt #ff001d";
+        
+        document.getElementById("convert").style.animationName = "color-change-warm";
+    }
+}
+
 //convertTemp(50,"C");
 
-
+//#000aff
 
 
 // (C × 9/5) + 32 = F;
